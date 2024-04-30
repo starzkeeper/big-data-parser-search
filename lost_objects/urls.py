0971c3858f20c1d16b5feca25638ec9013d9eff1
starzkeeper <yaroslav020304@gmail.com>
2024-04-30 15:26:54 +0300

    third commit

--- lost_objects/urls.py
+++ lost_objects/urls.py
@@ -1,4 +1,3 @@
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 from django.urls import path, include
 from search.views import HeritageLostObjectDocumentView
